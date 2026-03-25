5abbfb23dde683ccb3b967eb560522aa5ec4cffc
María De Tíscar García Navarrete <mtgn0001@red.ujaen.es>
2026-03-25 07:54:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4997575916-9dd77",
+  "appId": "1:1010807126798:web:7da24b8119ead835c9355f",
+  "apiKey": "AIzaSyABwXiBNHB-I5HipO-3U-dX7bIWc-_yx4Y",
+  "authDomain": "studio-4997575916-9dd77.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1010807126798"
+};
