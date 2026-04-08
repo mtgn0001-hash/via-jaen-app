cc5f4ec3c02570cbbe7b50bf4a5e18db890d54aa
María De Tíscar García Navarrete <mtgn0001@red.ujaen.es>
2026-04-08 07:01:57 +0000

    Despliega mi aplicación en Firebase Hosting

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,5 @@ export const firebaseConfig = {
   "appId": "1:1010807126798:web:7da24b8119ead835c9355f",
   "apiKey": "AIzaSyABwXiBNHB-I5HipO-3U-dX7bIWc-_yx4Y",
   "authDomain": "studio-4997575916-9dd77.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "1010807126798"
 };
